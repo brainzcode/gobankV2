8f5ad2c4b8a8cc593283c05862c611a5f7417dcd
brainzcode <98150590+brainzcode@users.noreply.github.com>
2023-01-12 06:15:11 +0100

    entry.sql, transfers.sql

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:brainzcode@localhost:5432/gobankV2?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to DB:", err)
+	}
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
